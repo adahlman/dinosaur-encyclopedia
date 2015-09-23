8567c060db1b13c75e753550fbda2bca77361280
Ali Dahlman <ali.dahlman57@gmail.com>
2015-09-23 15:00:58 -0400

    Neater query

--- lookup.php
+++ lookup.php
@@ -1,7 +1,15 @@
 <?php
-$s = $_GET['search'];
-    $url = "http://en.wikipedia.org/w/api.php?action=query&titles=".$s."&prop=extracts&exintro=true&format=json";
-    $ch = curl_init($url);
+    $s = $_GET['search'];
+    $url = "http://en.wikipedia.org/w/api.php";//?action=query&titles=".$s."&prop=extracts&exintro=true&format=json";
+    $params = array(
+                    'action'=>'query',
+                    'titles'=>$s,
+                    'prop'=>'extracts',
+                    'exintro'=>true,
+                    'format'=>'json'
+                    );
+                
+    $ch = curl_init($url."?".http_build_query($params));
     curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
     curl_setopt($ch, CURLOPT_POST, FALSE);
     curl_setopt($ch, CURLOPT_HEADER, false);
@@ -14,5 +22,4 @@ $s = $_GET['search'];
     curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; he; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8");
     $page = curl_exec($ch);
     exit($page);
-
 ?>
